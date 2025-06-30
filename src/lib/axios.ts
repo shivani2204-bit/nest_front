bd7e64fe9841ff3bf5bbf3b2e081c6f8fe723412
Shivani Jaisawal <jaiswalshivani.vhits@gmail.com>
2025-06-30 15:50:15 +0530

    clear commented part

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,18 +1,3 @@
-// import axios from 'axios';
-
-// const api = axios.create({
-//   baseURL: 'http://localhost:3001',
-// });
-
-// api.interceptors.request.use((config) => {
-//   const token = localStorage.getItem('token');
-//   if (token) config.headers.Authorization = `Bearer ${token}`;
-//   return config;
-// });
-
-// export default api;
-
-
 import axios from 'axios';
 import { useAuthStore } from '@/stores/useAuthStore';
 
--- src/stores/useAuthStore.ts
+++ src/stores/useAuthStore.ts
@@ -1,39 +1,3 @@
-// import { create } from 'zustand';
-
-// interface AuthState {
-//   token: string | null;
-//   permissions: string[];
-//   setAuth: (token: string, permissions: string[]) => void;
-//   clearAuth: () => void;
-// }
-
-// export const useAuthStore = create<AuthState>((set) => ({
-//   token: null,
-//   permissions: [],
-//   setAuth: (token, permissions) =>
-//     set({ token, permissions }),
-//   clearAuth: () => set({ token: null, permissions: [] }),
-// }));
-
-// stores/authStore.ts
-// import { create } from 'zustand';
-
-// type AuthState = {
-//   token: string | null;
-//   permissions: string[];
-//   setToken: (token: string) => void;
-//   setPermissions: (permissions: string[]) => void;
-//   logout: () => void;
-// };
-
-// export const useAuthStore = create<AuthState>((set) => ({
-//   token: null,
-//   permissions: [],
-//   setToken: (token) => set({ token }),
-//   setPermissions: (permissions) => set({ permissions }),
-//   logout: () => set({ token: null, permissions: [] }),
-// }));
-
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
